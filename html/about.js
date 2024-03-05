07d19d58739994076fc2e81fbc3c4e08c433c751
=piyali <=piyalichatterjee25082001@gmail.com>
2024-03-05 12:47:39 +0530

    Adding to gitHub

---
+++ html/about.js
@@ -0,0 +1,664 @@
+// alert("hello world");
+// document.getElementById("box").innerHTML="Priya";
+// document.write("Priya");
+// console.log(4+6);
+// // alert(4+6);
+// let a=8;
+// alert(a);
+// let x=25;
+// y=25;
+//  z =x+y;
+// console.log (x+5*2)
+//     let x =  "piyali";
+// console.log(typeof x)
+// let age = 16;
+// let hasvoterCard = "no";
+// if (age >= 15) {
+// console.log("you can vote")
+// }
+// else if (age >= 15)
+// console.log("get your voter id card")
+// else {
+// console.log("you cannot vote")
+// }
+// let isLoggedin=1;
+// if(isLoggedin==0){
+// document.write("login")
+// }
+// else{
+// document.write("logout")
+// }
+// let num =205
+// if(num>=50){
+// console.log(`${num} is a number` )
+// }
+// if input = 1, "y","yes" output = continue..
+// if input = 0, "n","no" output = end...
+// let input =1
+// if(input===1){
+//     document.write("continue")
+// }
+// else if(input==="y"){
+//     document.write("continue")
+// }
+// else if(input==="yes"){
+//     document.write("continue")
+// }
+// else if(input===0){
+//     document.write("end")
+// }
+// else if(input==="n"){
+//     document.write("end")
+// }
+// else if(input==="no"){
+//     document.write("end")
+// }
+// else{
+//     document.write("wrong no")
+// }
+// let number = 12;
+// switch (number) {
+//     case 12:
+//         console.log(`this number is ${number}`)
+//         break
+//     default:
+//         console.log("wrong no")
+//         break
+// }
+// let counter = 0;
+// while(counter <=10){
+//     console.log(counter);
+//     counter= counter +2;
+// }
+// let number = 1;
+// let sum =0;
+// while(number<=100){
+//     if(number%2==0){
+//         sum =sum +number
+//     }
+// number++;
+// console.log (sum);
+// }
+// for(let number=1;number<=100;number++){
+//     console.log(number);
+// }
+// for(let number=10;number<=100;number++){
+//     if(number %2==0){
+//     console.log(number)}
+// }
+// alert ("welcome....")
+//  let age=prompt("enter your age",10);
+//  if(age != null){
+//     document.write(`your age is ${age}`)
+//  }
+//  else{
+//     document.write("age field was blank")
+// }
+//  let response =confirm("are you sure,you want to delete?")
+// if(response){
+//     document.write("deleteed")}
+//     else{
+//     document.write(" not deleteed")
+// }
+// let day = "friday"
+// switch (day) {
+//     case "monday":
+//         console.log("4am")
+//         break
+//     case "tuesday":
+//         console.log("4am")
+//     case "wednesday":
+//         console.log("4am")
+//     case "thursday":
+//         console.log("6am")
+//         break
+//     case "friday":
+//         console.log("7am")
+//     case "staurday":
+//         console.log("10am")
+
+// }
+// let number = 2
+// switch (number) {
+//     case 1:
+//         console.log(number)
+//     case 2:
+//         console.log(number)
+//     case 3:
+//         console.log(number)
+//     case 4:
+//         console.log(number)
+
+// }
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 6) {
+//         break
+//     }
+//     console.log(i)
+
+// }
+// let amount = 10000
+// let time = 10
+// let rate = .1
+// let n = 4
+// console.log(amount*((1 + rate / n)**(n*time)))
+
+// f = (9/5 * c) + 32
+//c = (f - 32)*5/9
+// let f = 500
+// console.log((f-32)*5/9)
+// A = P(1 + r/n)^nt
+//A-P=(1+r/n)^nt
+//or//
+//P=(A/(1+r/n)^nt
+// let type = "5" * "yes"
+// console.log(typeof type)
+// console.log(type)
+// let type = "kk"
+// console.log(typeof type)
+// let NewType = String(type)
+// console.log(typeof NewType) 
+// console.log(NewType)
+// let name = "piyali"
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(0,3))
+// console.log(name.replace("piyali","priya"))
+// let friend = "abhradip"
+// console.log(name+ " " +friend)
+// console.log(name.concat(" is a friend of ", friend ))
+// let str = "piyali"
+// let str2 = "chatterjee"
+//  let str4 = "priya"
+// let str3 = str.concat(str2,str4)
+// console.log(str3)
+// let str = "This is Javascript tutorial.It is a good tutorial "
+// let substring=str.substr(8,19)
+// console.log(substring)
+// let str = "This is Javascript tutorial. It is a good tutorial "
+// let position = str.lastIndexOf("good")
+// console.log(position)
+// let str="This is Javascript tutorial. It is a good tutorial"
+// let str = " This is Javascript tutorial. It is a good tutorial   "
+// let str2 = str.includes("good")
+// let trimedStr = str.trimEnd()
+// console.log(str2)
+// console.log(trimedStr)
+// let book1 ="Math"
+// let book2 ="Physics"
+// let book3 ="Bio"
+// let book4 ="Computer Science"
+// console.log(book4)
+// let asset =[1,5,6,7,8,"piyali",4]
+// console.log(asset[5])
+// let book ="Math"
+// let text ="This is a random text"
+// let wordarray=text.split(" ")
+// console.log(wordarray)
+// let book =["Math","Physics","Bio","Computer Science"]
+// let bookWithPages=[
+//     ["Math,100"],
+//     ["Bio","200"]
+// ]
+// let book4=bookWithPages[1][0]
+// console.log(book4)
+// book[2]="bengali"
+// book.length=0   //empty Array//
+// let position=book.indexOf("Physics")
+// console.log(position)
+// function MulTable(num){
+//     for(let i=1;i<=10;i++){
+//         document.write(`${num}* ${i}=${num*i}`)
+//         document.write("<br>")
+//     }
+//     document.write("<br><br>")
+// }
+// MulTable(2)
+// document.write("hello")
+// document.write("<br>")
+
+// MulTable(15)
+// function add(num1,num2){
+//     console.log(num1+num2)
+// }
+// add(12,6)
+// function num (){
+//     for(let i=1;i<=100;i+=2){
+//        if(i==51){
+//            break
+
+//        }
+//         console.log(i)
+// //         // console.log("<br>")
+
+//     }
+// }
+// num()
+// let text ="Here are some of the most Kolkata famous dishes that you can try out on your next visit to the city"
+// let text2=text.split(" ")
+// console.log(text2)
+// function add(arr){
+//     for(let i=0;i <arr.length;i++) {
+//         console.log(arr[i])
+//     }
+// }
+// add({
+//     name : "abc",
+//     age : 22,
+
+// })
+// function add(){
+//     let sum=0
+//     for (let i=2;i<=100;i+=2){
+//       sum += i 
+//     }
+//     console.log(sum)
+
+// }
+// add()
+// function add() {
+//   let sum = 0
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 10 == 0) {
+//       sum += i
+//       console.log(i)
+//     }
+//   }
+//   for (let i = 10; i <= 100; i += 10) {
+
+//     sum += i
+//     // console.log(i)
+
+//   }
+//   console.log(sum)
+// }
+// add()
+
+// let text="In the equation for the sum of even numbers"
+// let text2=text.split("")
+// console.log(text2)
+// console.log(text2.length)
+// let size = 0
+// for(let i=0;i<text2.length;i++){
+//     console.log(text2[i])
+//     if(text2[i] !== " "){
+//         size++
+//     }
+// }
+// console.log(size)
+// let temp = 0
+// let size2 = text.length
+// for(let i=0;i<size2;i++) {
+//     if(text[i] !== " "){
+//         temp++
+//     }
+// }
+// console.log(temp)
+// let text3 = text.replaceAll(" ","")
+// console.log(text3.length)
+// function textLength(text){
+//     let text3 = text.replaceAll(" ","")
+//     console.log(text3.length)
+
+// } 
+// let text = "In the equation for the sum of even numbers"
+// textLength(text)
+// let arr=[200,8,1,9,4,76,305,69,0]
+// let bigNumber=0
+// for(let i=0;i<arr.length;i++){
+//     if(bigNumber<arr[i]){
+//         // console.log(bigNumber)
+//         bigNumber=arr[i]
+//     }
+//   }
+ // console.log(bigNumber)
+
+// 5! = 5*4*3*2*1
+// let num=5
+// let num2=1
+// for(let i=2;i<=num;i++){
+//     num2*=i
+
+
+// }
+// console.log(num2)
+// function add(num){
+//     console.log(num)
+// }
+// add({name:'piyali'})
+
+//  function multiplication(num){
+//     // let num = 5
+//     let num2=1
+//     for(let i=1;i<=num;i++){
+//         num2*=i
+//     }
+//     console.log(num2)
+// }
+// let num=5
+// multiplication(num)
+// let num =5
+// let num2=1
+// while(num>=2){
+//     num2*=num
+//     num--
+// }
+// console.log(num2)
+
+// let num=36
+// let num2=2
+// let check=true
+// while(num2<num){
+//     if(num%num2==0){
+//         check=false
+//     }
+//     num2++
+// }
+// if(check){
+//     console.log("This number is prime")
+// }
+// else{
+//     console.log("This number is not prime")
+// }
+
+
+// let num=512345678 // 26
+// let check = true
+// for( let i=2;i<Math.floor(num/2);i++){
+//     console.log(i)
+//     if(num%i == 0){
+//         check = false
+//         break
+//     }
+// }
+// if(check){
+//     console.log("This number is prime")
+// }
+// else {
+//     console.log("This number is not prime")
+// }
+// function add(a,b){
+//     let d =[4,8,6,4]
+//     return d
+// }
+// let c = add(5,5)
+// console.log(c)
+
+// document.write(c)
+// function compare(a,b){
+//     if(a>b){
+//         return a
+//     }
+//     else if(a<b){
+//         return b
+
+//     }
+//     else{
+//         return "Variables are equal"
+//     }
+// }
+// let c=compare(6,8)
+// document.write(c)
+// let num=[20,3,5,10,60,75,4,90,2]
+// function sub(a,b){
+// return  b-a
+// }
+// console.log(num)
+// console.log(num.sort(sub))
+// let num=5
+// function add(){
+// let num=2
+
+// console.log(num)
+// }
+// function sub(){
+//     let num=50
+//     console.log(num)
+// }
+// add()
+// sub()
+
+//  let show = function(){
+//     console.log("Hi")
+// }
+// show()
+// function name() {
+
+// }
+// const name = function() {
+
+// }
+// const name = () =>{
+
+// }
+
+// ((num1,num2)=>{
+//     console.log(num1+num2)
+// })(4,5)
+
+// let person = {
+//     firstName:"Piyali",
+//     lastName:"Chatterjee",
+//     sayHello : (name) => {
+//         console.log(`Hi ${name}`)
+//     }
+// }
+// person.age=45
+// person.firstName="priya"
+// delete person.lastName
+// console.log("class" in person)
+// for(let i in person){
+// console.log(i+":"+person[i])
+// console.log(`${i}  :  ${person[i]} `)
+// }
+// person.sayHello("Piyali beats Abhradip")
+//   Math Object  //
+// console.log(Math.PI)   
+// console.log(Math.E)
+
+// let x =Math.round (5.2)
+// console.log(x) 
+// let y =Math.ceil (50.1)
+// console.log(y)   
+// let y =Math.floor(5.2)
+// console.log(y)   
+// let y =Math.trunc(4.3)
+// console.log(y)   
+// let y =Math.pow(2,3)
+// console.log(y)   
+// let y =Math.sqrt(4)
+// console.log(y)   
+// let y =Math.min(4,8,7,89,7)
+// console.log(y)   
+// let y =Math.max(4,8,7,89,7)
+// let num=Math.random()
+
+// console.log(num)
+
+// console.log(y)   
+// let randomNumber = Math.floor(Math.random() * 6) + 1
+// console.log(randomNumber)
+
+// const obj = {
+//     name: "Piyali",
+//     age : 22,
+//     weight : 55,
+//     bloodgroup : "O-",
+//     id: `${Date.now()}_${ Math.ceil(Math.random() * 6)}`,
+//     registerdate : new Date().toLocaleString()
+// }
+// console.log(obj)
+
+
+
+
+
+// let arr1 = [10, 20, 30,40]
+// let num = 0
+// for (let i = 0; i < arr1.length; i++) {
+//   num += arr1[i]
+// }
+// // console.log(num)
+
+// function arr2(arr1) {
+//   let num = 0
+//   for (let i = 0; i < arr1.length; i++) {
+//     num += arr1[i]
+//   }
+//   return num
+//   // console.log(num)
+  
+// }
+//  let num1 = arr2(arr1)
+// console.log(num)
+// let num3=0
+// while(num<arr1.length){
+//   num3 += arr1[num]
+//   num++
+// }
+// console.log(num3)
+
+
+
+
+
+// function multiplication(num){
+  //     // let num = 5
+  //     let num2=1
+  //     for(let i=1;i<=num;i++){
+  //         num2*=i
+  //     }
+  //     console.log(num2)
+  // }
+  // let num=5
+  // multiplication(num)
+  // let num =5
+  // let num2=1
+  // while(num>=2){
+  //     num2*=num
+  //     num--
+  // }
+  // console.log(num2)
+  
+
+// arr1.length
+// arr1.concat()
+// let str = "hello how are you"
+// // let str = arr1.splice(2,3,10,20,30)
+// let str1 = str.slice(2,9)
+// console.log(str1)
+// console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function add(num1,num2){
+//     // console.log(num1+num2)
+//     return num1+num2
+//
+// }
+// console.log(add(1,2)
+
+// let studentResult = [
+//     {
+//         name: "Piyali Chatterjee",
+//         age: 22,
+//         section: "A",
+//         marks: 50,
+//     },
+//     {
+//         name: "Kuheli Chatterjee",
+//         age: 21,
+//         section: "A",
+//         marks: 80,
+//     },
+//     {
+//         name: "Abhradip Paul",
+//         age: 21,
+//         section: "A",
+//         marks: 70,
+//     },
+//     {
+//         name: "Puja Saha",
+//         age: 22,
+//         section: "A",
+//         marks: 40,
+//     },
+//     {
+//         name: "Pubali Chatterjee",
+//         age: 20,
+//         section: "A",
+//         marks: 60,
+//     }
+// ]
+// console.log(studentResult[2])
+// for (let i = 0; i < studentResult.length; i++) {
+//     // console.log(studentResult[i].name)
+//     let marks = studentResult[i].marks
+//     if (marks >= 70) {
+//         console.log(studentResult[i].marks)
+//     }
+// }
+
+
+// let num= 4
+// let num2=2
+// num+=num2
+// console.log(num)
+// console.log(num2)
+// console.log(num3)
+
+// let student = [
+//     {
+//         name: "Piyali Chatterjee",
+//         age: 20,
+//         rollno: 25,
+//         class: "one"
+//     },
+//     {
+//         name: "Puja Saha",
+//         age: 20,
+//         rollno: 24,
+//         class: "one"
+
+//     },
+//     {
+//         name: "Kuheli Chatterjee",
+//         age: 20,
+//         rollno: 23,
+//         class: "one"
+
+//     },
+//     {
+//         name: "Pubali Chatterjee",
+//         age: 20,
+//         rollno: 22,
+//         class: "one"
+
+//     }
+// ]
+// console.log(student[2])
+// for ( let i=0;i<student.length;i++){
+//     console.log(student[i].rollno)
+//     // let rollno = student[i].rollno
+//     if(rollno >= 24){
+//         console.log(student[i].name)
+//     }
+// }
+// for (let i=student.length-1;i>=0;i--){
+//     console.log(student[i].name)
+// }
---
+++ html/harry.js
@@ -0,0 +1,245 @@
+// Chapter -1 //
+// 1. crate a variable of type sting and try to add a number to it ?
+// let num1 = "piyali"
+// let num2= 74
+// console.log(num1 +" "+ num2)
+
+// 2. Use typeof opreratar to find the datatype of the String n last question?
+// let num1 = "piyali"
+// let num2= 74
+// console.log(typeof num1)
+// console.log(typeof num2)
+// 3.cratea const object in javascript can you change it to hold a number later?
+// const obj = {
+//     name: "Piyali",
+//     age: 5
+// }
+// obj ={
+//     age :5
+// }
+
+// 4.Try to add new key to the const object in problem 3 were you  to do it?
+// const obj = {
+//         name: "Piyali",
+//          age: 5
+//      }
+//      obj["friend"] = "Abhradip"
+//      console.log(obj)
+
+// 5.write a js program to create a ward mcaning dictionary of words?
+// let dict={
+//     Immediately:"at once,right away",
+//     Apreciate:"to understand the worth or importance of (something or someone)",
+//     Apologize:"to express regret for something done or said ",
+//     Depression:"a common mental disorder",
+//     decision:" a judgment, conclusion, or resolution reached or given; verdict"
+// }
+// console.log(dict.Apologize)
+// Chapter -2 //
+// 1.use logical opertors to fine whether the age og a person lies between 10 and 20?
+// let age= 42
+// if(age>=10 && age<=20){
+//     console.log("Your age lies between 10 and 20")
+// }
+// else{
+
+//  console.log("Your age doesnt lies between 10 and 20")
+// }
+// 2.demonstrate the use of switch case statements in javascript
+
+// let age = 18
+// switch (age) {
+//     case 10:
+//         console.log("Your age is 10")
+//         break
+//     case 11:
+//         console.log("Your age is 11")
+//         break
+//     case 12:
+//         console.log("Your age is 12")
+//         break
+//     case 13:
+//         console.log("Your age is 13")
+//         break
+//     case 14:
+//         console.log("Your age is 14")
+//         break
+//     case 15:
+//         console.log("Your age is 15")
+//         break
+//     default:
+//         console.log("Your age is not special")
+// }
+// 3.Write a javascript program to find whether a Number is divisible by 2 and 3
+/*
+let num = 12
+if (num % 2 == 0 && num % 3 == 0) {
+    console.log("Your number is divisible by 2 and 3")
+}
+else {
+
+    console.log("Your number is not divisible by 2 and 3")
+}
+*/
+// 4.Write a javascript program to find whether a Number is divisible by 2 or 3
+// let num = 10
+// if (num % 2 == 0 || num % 3 == 0) {
+//     console.log("Your number is divisible by 2 and 3")
+// }
+// else {
+
+//     console.log("Your number is not divisible by 2 and 3")
+// }
+// 5.print "you can Drive "or "you cannot Drive" based on age qreater than 18 using ternary opertor
+// let age = 78
+// if (age >= 18) {
+//     console.log("you can drive")
+// }
+// else {
+//     console.log("you cannot drive")
+// }
+// Chapter -3 //
+// 1. Write a program to print the marks of a student in an object using for loop?
+/*
+let student={
+    piyali:90,
+    abhradip:95,
+    puja:87,
+    kuheli:85,
+    pubali:50
+}
+
+for (i=0;i<Object.keys(student).length;i++){
+    console.log("The marks of " + Object.keys(student)[i] + " are " + student[Object.keys(student)[i]])
+}
+
+// 2.Write the program in Q1 using for in loop?
+
+for (let i in student){
+    console.log("The marks of " + i + " are "+ student[i])
+}
+*/
+// 3.Write a program to print "try aging" until the uses the correct number?
+// let num = 50
+// let a 
+// while (a != num) {
+//     console.log("try aging")
+//     a = prompt("enter a number")
+// }
+// console.log("you have entered a correct number")
+// 4.write a function to find mean of 5 Number?
+
+// function mean(a,b,c,d,e,f){
+//     return (a+b+c+d+e+f)/6
+// }
+
+// console.log(mean(40,55,66,74,84,93))
+
+// let mean1=Math.floor(mean(40,55,66,74,84,93))
+// console.log(mean1)
+// Chapter -4 //
+// 1. what will the following print in javasctipt?
+// let str="piyali\""
+// console.log(str.length)
+// 2.explore the includes starts with & ends with functions tof a string?
+// let str="Serampore College is located in Serampore, in West Bengal state, India. Established in 1818, it is the third oldest college in the country after CMS College, Kottayam and Presidency College "
+// let word="Serampore"
+// console.log(str.includes(word))
+// console.log(str.startsWith(word))
+// console.log(str.includes(word))
+
+
+// 3.write a program to convert a given string to lowercase?
+// let str="Durga Puja is a famous Hindu festival when Goddess Durga is worshipped. Durga Puja is also known as Durgotsava. Durgotsava refers to all five day"
+// console.log(str.toLowerCase())
+// 4.extact the amout out of this sting "Please five Rs 1000"?
+// let str="Please give Rs 1000"
+//  let amount=str.slice("Please give Rs".length)  //1//
+// let amount=str.slice(15)  //2//
+// console.log(amount)
+// console.log(typeof amount)
+// let srt = Number.parseInt(amount)
+// console.log( typeof srt)
+// 4.Try to change 4th character of a given string were you able to do it?
+// let friend="puja"
+// friend[2]="k"
+// console.log(friend)  // friend is not change because srting is immutable\\
+// Chapter -5 //
+//1. Crate an array of number and take input from the user to add numbers to this array?
+// let arr=[4,5,6,2]
+// let num=prompt("enter a number")
+// num=Number.parseInt(num)
+// arr.push(num)
+// console.log(arr)
+// 2.keep adding number to the array in 1 until 0 is added to the array?
+//  let arr=[4,5,6,3]
+//  let a
+// do {
+//    a=prompt("enter a number")
+//    a=Number.parseInt(a)
+//     arr.push(a)
+// }while(a!=0)
+// console.log(arr)
+// 3.filler for Numbers divisible by 10 from a given array?
+// let arr = [4, 50, 90, 60, 3, 2, 70, 60, 5, 50]
+// let arr2 = arr.filter((num) => {
+//     return num %10===0
+//     // console.log(num)
+// })
+// console.log(arr2)
+// 4.crete an array og square of given numbers?
+// let arr = [4, 50, 90, 60, 3, 2, 70, 60, 5, 50]
+// let arr2 = arr.map((num) => {
+//     // return num*num     //1//
+//     return num**2          //2//
+//     console.log(num)
+// })
+// console.log(arr2)
+// 5.use reduce to calcutate factorial og given number from an array of first n natural numbers (n being the number where fational needs  to be calcutate)?
+
+// let arr=[4,5,3,2,8,6]
+// let arr2=arr.reduce((num1,num2)=>{
+//     return num1*num2
+// })
+// console.log(arr2)
+
+
+
+
+
+
+
+
+// let student = [
+//     {
+//         name: "Piyali Chatterjee",
+//         age: 20,
+//         rollno: 25,
+//         class: "one"
+//     },
+//     {
+//         name: "Puja Saha",
+//         age: 20,
+//         rollno: 24,
+//         class: "one"
+
+//     },
+//     {
+//         name: "Kuheli Chatterjee",
+//         age: 20,
+//         rollno: 23,
+//         class: "one"
+
+//     },
+//     {
+//         name: "Pubali Chatterjee",
+//         age: 20,
+//         rollno: 22,
+//         class: "one"
+
+//     }
+// ]
+// // console.log(student[2].age)
+// for (let i = 0; i < student.length; i++) {
+//     console.log(student[i].rollno)
+// }
